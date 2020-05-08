55200cd19300802fb42d43332866560100fa2413
lls <ryannotdead@gmail.com>
2020-05-08 16:25:45 +0800

    day1

---
+++ LPTHW/ex5.py
@@ -0,0 +1,21 @@
+my_name = "Jaden Wang"
+my_age = 17 # a lie
+my_height = 180
+my_weight = 60
+my_eyes = "Brown"
+my_teeth = "White"
+my_hair = "Black"
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm high.")
+print(f"He's {my_weight} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+print(round(1.48))
+print(round(1.51))
+print(round(my_weight/3))
