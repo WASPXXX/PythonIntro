afc6f143562121503920bfdcfbd20b7770abdeeb
lls <ryannotdead@gmail.com>
2020-05-12 12:17:35 +0800

    day2

---
+++ LPTHW/ex11.py
@@ -0,0 +1,16 @@
+print("How old are you?")
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} yrs old, {height} tall and {weight} heavy.")
+
+print("aaaaaaa", end='')
+print("bõää", end='')
+print("\n")
+
+# convert to string instead of the way it is
+x = input('type a number -->')
+print(x * 2)
