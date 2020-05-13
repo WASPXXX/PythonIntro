7970fbf5ffeb85f713323c742e4c31646cb14e6c
lls <ryannotdead@gmail.com>
2020-05-14 05:42:55 +0800

    day2

--- LPTHW/ex16.py
+++ LPTHW/ex16.py
@@ -9,10 +9,11 @@ print("If you do want that, hit RETURN.")
 input("?")
 
 print("Opening the file...")
-target_file = open(filename, 'w')
+# target_file = open(filename, 'w')
+target_file = open(filename, 'r+')
 
 print("Truncating the file. Goodbye!")
-target_file.truncate()
+# target_file.truncate()
 
 print("Now I'm gonna ask you for 3 lines.")
 
@@ -22,12 +23,13 @@ line3 = input("line 3: ")
 
 print("I'm gonna write these lines to the file.")
 
-target_file.write(line1)
-target_file.write('\n')
-target_file.write(line2)
-target_file.write('\n')
-target_file.write(line3)
-target_file.write('\n')
+# target_file.write(line1)
+# target_file.write('\n')
+# target_file.write(line2)
+# target_file.write('\n')
+# target_file.write(line3)
+# target_file.write('\n')
+target_file.write(f"{line1}\n{line2}\n{line3}\n")
 
 print("And finally, we close it.")
 target_file.close()
