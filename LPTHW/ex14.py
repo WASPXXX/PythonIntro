3f109e78964801df35732a2609005700a93fdad0
lls <ryannotdead@gmail.com>
2020-05-14 04:20:32 +0800

    day2

---
+++ LPTHW/ex14.py
@@ -0,0 +1,21 @@
+from sys import argv
+
+script, user_name, country, age = argv
+prompt = '--> '
+
+print(f"Hi {age} yrs old {user_name} from {country}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computers do you have?")
+computer = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Cool.
+""")
---
+++ LPTHW/ex16.py
@@ -0,0 +1,34 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We are going to erase {filename}.")
+print("If you don't want that, hit Ctrl-C(^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target_file = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target_file.truncate()
+
+print("Now I'm gonna ask you for 3 lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print("I'm gonna write these lines to the file.")
+
+target_file.write(line1)
+target_file.write('\n')
+target_file.write(line2)
+target_file.write('\n')
+target_file.write(line3)
+target_file.write('\n')
+
+print("And finally, we close it.")
+target_file.close()
+print("If it's closed? ",target_file.closed)
